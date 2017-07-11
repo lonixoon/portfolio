9228740cd2225957a7d62ced075e63402f26019d
lonixoon <soinov.alexey@yandex.ru>
2017-07-11 22:25:37 +0700

    баги

--- build/js/main.min.js
+++ build/js/main.min.js
@@ -18,6 +18,23 @@ request.onload = function() {
 
 request.send();
 
+ //-Открывашка для главного меню-----------------------------
+var menuToggle = document.querySelector(".main-nav__toggle");
+var menuClosed = document.querySelector(".main-nav__list");
+
+menuToggle.addEventListener("click", function (event) {
+    event.preventDefault();
+    menuClosed.classList.toggle("main-nav__list--opened");
+    menuToggle.classList.toggle("main-nav__toggle--active");
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        menuClosed.classList.remove("main-nav__list--opened");
+        menuToggle.classList.toggle("main-nav__toggle--active");
+    }
+});
+
 
 //-Слайдер---------------------------------------------------
 var sliderItem1 = document.querySelector(".my-work__item1");
@@ -73,35 +90,17 @@ backSlideItem3.addEventListener("click", function (event) {
 });
 
 
- //-Открывашка для главного меню-----------------------------
-var menuToggle = document.querySelector(".main-nav__toggle");
-var menuClosed = document.querySelector(".main-nav__list");
-
-menuToggle.addEventListener("click", function (event) {
-    event.preventDefault();
-    menuClosed.classList.toggle("main-nav__list--opened");
-    menuToggle.classList.toggle("main-nav__toggle--active");
-});
-
-window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-        menuClosed.classList.remove("main-nav__list--opened");
-        menuToggle.classList.toggle("main-nav__toggle--active");
-    }
-});
-
-
 //-Открывашка для навигации по станицы-----------------------
-var pageToggle = document.querySelector(".page-nav__toggle");
-var pageClosed = document.querySelector(".page-nav__wrap");
+// var pageToggle = document.querySelector(".page-nav__toggle");
+// var pageClosed = document.querySelector(".page-nav__wrap");
 
-pageToggle.addEventListener("click", function (event) {
-    event.preventDefault();
-    pageToggle.classList.toggle("page-nav__toggle--opened");
-    pageClosed.classList.toggle("page-nav__wrap--opened");
-});
+// pageToggle.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     pageToggle.classList.toggle("page-nav__toggle--opened");
+//     pageClosed.classList.toggle("page-nav__wrap--opened");
+// });
 
-pageClosed.addEventListener("click", function (event) {
-    pageToggle.classList.toggle("page-nav__toggle--opened");
-    pageClosed.classList.toggle("page-nav__wrap--opened");
-});
+// pageClosed.addEventListener("click", function (event) {
+//     pageToggle.classList.toggle("page-nav__toggle--opened");
+//     pageClosed.classList.toggle("page-nav__wrap--opened");
+// });
