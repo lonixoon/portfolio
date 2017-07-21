f6f53c01ec5a58bbf41fce7d48a2469d9787cb10
lonixoon <soinov.alexey@yandex.ru>
2017-07-21 16:57:32 +0700

    Делаем фиксированное меню в блоге

--- build/js/main.min.js
+++ build/js/main.min.js
@@ -23,99 +23,99 @@ request.send();
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 //////////////  Открывашка для главного меню  //////////////////////////
-var menuToggle = document.querySelector(".main-nav__toggle");
-var menuClosed = document.querySelector(".main-nav__list");
+var menuToggle = document.querySelector('.main-nav__toggle');
+var menuClosed = document.querySelector('.main-nav__list');
 
 if (menuToggle) {
-    menuToggle.addEventListener("click", function (event) {
+    menuToggle.addEventListener('click', function (event) {
         event.preventDefault();
-        menuClosed.classList.toggle("main-nav__list--opened");
-        menuToggle.classList.toggle("main-nav__toggle--active");
+        menuClosed.classList.toggle('main-nav__list--opened');
+        menuToggle.classList.toggle('main-nav__toggle--active');
     });
 }
 
-window.addEventListener("keydown", function(event) {
+window.addEventListener('keydown', function(event) {
     if (event.keyCode === 27) {
-        menuClosed.classList.remove("main-nav__list--opened");
-        menuToggle.classList.toggle("main-nav__toggle--active");
+        menuClosed.classList.remove('main-nav__list--opened');
+        menuToggle.classList.toggle('main-nav__toggle--active');
     }
 });
 
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 //////////////  Слайдер  ///////////////////////////////////////////////
-var sliderItem1 = document.querySelector(".my-work__item1");
-var sliderItem2 = document.querySelector(".my-work__item2");
-var sliderItem3 = document.querySelector(".my-work__item3");
-var nextSlideItem1 = document.querySelector(".slider__next--barbershop");
-var backSlideItem1 = document.querySelector(".slider__back--nerds");
-var nextSlideItem2 = document.querySelector(".slider__next--nerds");
-var backSlideItem2 = document.querySelector(".slider__back--sedona");
-var nextSlideItem3 = document.querySelector(".slider__next--sedona");
-var backSlideItem3 = document.querySelector(".slider__back--barbershop");
+var sliderItem1 = document.querySelector('.my-work__item1');
+var sliderItem2 = document.querySelector('.my-work__item2');
+var sliderItem3 = document.querySelector('.my-work__item3');
+var nextSlideItem1 = document.querySelector('.slider__next--barbershop');
+var backSlideItem1 = document.querySelector('.slider__back--nerds');
+var nextSlideItem2 = document.querySelector('.slider__next--nerds');
+var backSlideItem2 = document.querySelector('.slider__back--sedona');
+var nextSlideItem3 = document.querySelector('.slider__next--sedona');
+var backSlideItem3 = document.querySelector('.slider__back--barbershop');
 
 if (nextSlideItem1) {
-    nextSlideItem1.addEventListener("click", function (event) {
+    nextSlideItem1.addEventListener('click', function (event) {
         event.preventDefault();
-        sliderItem1.classList.remove("my-work__item--show");
-        sliderItem2.classList.add("my-work__item--show");
-        sliderItem3.classList.remove("my-work__item--show");
+        sliderItem1.classList.remove('my-work__item--show');
+        sliderItem2.classList.add('my-work__item--show');
+        sliderItem3.classList.remove('my-work__item--show');
     });
 
-    backSlideItem1.addEventListener("click", function (event) {
+    backSlideItem1.addEventListener('click', function (event) {
         event.preventDefault();
-        sliderItem1.classList.remove("my-work__item--show");
-        sliderItem2.classList.remove("my-work__item--show");
-        sliderItem3.classList.add("my-work__item--show");
+        sliderItem1.classList.remove('my-work__item--show');
+        sliderItem2.classList.remove('my-work__item--show');
+        sliderItem3.classList.add('my-work__item--show');
     });
 
-    nextSlideItem2.addEventListener("click", function (event) {
+    nextSlideItem2.addEventListener('click', function (event) {
         event.preventDefault();
-        sliderItem1.classList.remove("my-work__item--show");
-        sliderItem2.classList.remove("my-work__item--show");
-        sliderItem3.classList.add("my-work__item--show");
+        sliderItem1.classList.remove('my-work__item--show');
+        sliderItem2.classList.remove('my-work__item--show');
+        sliderItem3.classList.add('my-work__item--show');
     });
 
-    backSlideItem2.addEventListener("click", function (event) {
+    backSlideItem2.addEventListener('click', function (event) {
         event.preventDefault();
-        sliderItem1.classList.add("my-work__item--show");
-        sliderItem2.classList.remove("my-work__item--show");
-        sliderItem3.classList.remove("my-work__item--show");
+        sliderItem1.classList.add('my-work__item--show');
+        sliderItem2.classList.remove('my-work__item--show');
+        sliderItem3.classList.remove('my-work__item--show');
     });
 
-    nextSlideItem3.addEventListener("click", function (event) {
+    nextSlideItem3.addEventListener('click', function (event) {
         event.preventDefault();
-        sliderItem1.classList.add("my-work__item--show");
-        sliderItem2.classList.remove("my-work__item--show");
-        sliderItem3.classList.remove("my-work__item--show");
+        sliderItem1.classList.add('my-work__item--show');
+        sliderItem2.classList.remove('my-work__item--show');
+        sliderItem3.classList.remove('my-work__item--show');
     });
 
-    backSlideItem3.addEventListener("click", function (event) {
+    backSlideItem3.addEventListener('click', function (event) {
         event.preventDefault();
-        sliderItem1.classList.remove("my-work__item--show");
-        sliderItem2.classList.add("my-work__item--show");
-        sliderItem3.classList.remove("my-work__item--show");
+        sliderItem1.classList.remove('my-work__item--show');
+        sliderItem2.classList.add('my-work__item--show');
+        sliderItem3.classList.remove('my-work__item--show');
     });
 }
 
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 //////////////  Открывашка для навигации по станицы  ///////////////////
-var pageToggle = document.querySelector(".page-nav__toggle");
-var pageClosed = document.querySelector(".page-nav__wrap");
+var pageToggle = document.querySelector('.page-nav__toggle');
+var pageClosed = document.querySelector('.page-nav__wrap');
 
 if (pageToggle) {
-    pageToggle.addEventListener("click", function (event) {
+    pageToggle.addEventListener('click', function (event) {
         event.preventDefault();
-        pageToggle.classList.toggle("page-nav__toggle--opened");
-        pageClosed.classList.toggle("page-nav__wrap--opened");
+        pageToggle.classList.toggle('page-nav__toggle--opened');
+        pageClosed.classList.toggle('page-nav__wrap--opened');
     });
 }
 
 if (pageClosed) {
-    pageClosed.addEventListener("click", function (event) {
-        pageToggle.classList.toggle("page-nav__toggle--opened");
-        pageClosed.classList.toggle("page-nav__wrap--opened");
+    pageClosed.addEventListener('click', function (event) {
+        pageToggle.classList.toggle('page-nav__toggle--opened');
+        pageClosed.classList.toggle('page-nav__wrap--opened');
     });
 }
 
@@ -126,7 +126,7 @@ var formElemen = document.querySelector(".feedback__form");
 
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
-//////////////  Визуализация preloader страницы  //////////////////////
+//////////////  Визуализация предзагрузки страницы  ////////////////////
 $(document).ready(function () {
     // console.log('ready');
 
@@ -180,7 +180,8 @@ $(document).ready(function () {
                 $('.page__header').css('display', 'flex');
                 $('.page__main').css('display', 'flex');
                 $('.page__footer').css('display', 'flex');
-                $('.preloader').css('display', 'none');
+                $('.preloader').hide();
+
             }
 
             $('.preloader__bar').css({ // выбираем элемет прелодер
@@ -190,4 +191,20 @@ $(document).ready(function () {
 
     });
 
+});
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//////////////  Приклеенное боковое меню  //////////////////////////////
+$(window).scroll(function() {
+    var wScroll = $(window).scrollTop();  // проверка на сколько px мы проскролили страницу
+    var menu = $('.page__static .page-nav__wrap');
+    var sidebar = $('.page__static .page-nav');
+    var stickyStart = sidebar.offset().top; // отслеживаем положение меню от верха страницы
+
+    if (wScroll >= stickyStart) { // если меню ниже чем верх страницы
+        menu.css({
+            top : wScroll - stickyStart + 'px' // опускаем меню на разницу px
+        });
+    }
 });
